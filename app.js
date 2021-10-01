1921b71c0a92dce8fd3aac819700b22908bc9478
zain <31380073+zainstar@users.noreply.github.com>
2021-10-01 09:21:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const app = express();
-const ig = require('instagram-scraping');
-const NodeCache = require('node-cache');
-const cache = new NodeCache({ stdTTL: 60 * 60 * 24});
+// const ig = require('instagram-scraping');
+// const NodeCache = require('node-cache');
+// const cache = new NodeCache({ stdTTL: 60 * 60 * 24});
 
-const verifyCache = (req, res, next) => {
-  try{
-    const {username} = req.params;
-    if(cache.has(username)){
-      return res.status(200).json(cache.get(username))
-    }
-    return next();
-  } catch (err) {
-    throw new Error(err)
-  }
-}
+// const verifyCache = (req, res, next) => {
+//   try{
+//     const {username} = req.params;
+//     if(cache.has(username)){
+//       return res.status(200).json(cache.get(username))
+//     }
+//     return next();
+//   } catch (err) {
+//     throw new Error(err)
+//   }
+// }
 
-app.get('/:username', verifyCache, async (req, res) => {
-  try {
-    const {username} = req.params
-    ig.scrapeUserPage(username).then((result) => {
-      cache.set(username, result);
-      return res.status(200).send(result)
-    });
-  } catch ({response}){
-    return res.sendStatus(500)
-  }
-});
+// app.get('/:username', verifyCache, async (req, res) => {
+//   try {
+//     const {username} = req.params
+//     ig.scrapeUserPage(username).then((result) => {
+//       cache.set(username, result);
+//       return res.status(200).send(result)
+//     });
+//   } catch ({response}){
+//     return res.sendStatus(500)
+//   }
+// });
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 
 app.listen(3000, () => {
     // console.log('My rest api running on port 3000')
